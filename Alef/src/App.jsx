8f4f28a3eb376b4f7db69a84cd886b00f63d7d5e
huda <hudaaltabbal433@gmail.com>
2026-05-01 17:29:25 +0300

    update

---
+++ Alef/src/App.jsx
@@ -0,0 +1,15 @@
+
+import HomePage from "./Pages/HomePage"
+import TutorPage from "./Pages/TutorsPage"
+import  Profile from "./Pages/Profile"
+import CreateStudentAccount from "./Pages/CreateStudentAccount"
+import OTP from "./Pages/OTP"
+function App() {
+  
+
+  return (
+    <OTP/>
+  )
+}
+
+export default App
