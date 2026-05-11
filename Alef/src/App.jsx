b4e709bfb5c74887eaa73e7f18d754e56a8d7a05
huda <hudaaltabbal433@gmail.com>
2026-05-11 11:35:55 +0300

    update

--- Alef/src/App.jsx
+++ Alef/src/App.jsx
@@ -1,24 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
-import TutorPage from "./Pages/TutorsPage";
+import TutorsPage from "./Pages/TutorsPage"
 import Profile from "./Pages/Profile";
 import FavPage from "./Pages/FavPage";
 import Notifications from "./Pages/notification";
 
 
+import SelectionPage from "./Pages/SelectionPage";
 import CreateStudentAccount from "./Pages/CreateStudentAccount";
 import OTP from "./Pages/OTP";
 
 function App() {
   return (
     <Routes>
-      
-      <Route path="/" element={<CreateStudentAccount />} />
+      <Route path="/" element={<SelectionPage />} />
+      <Route path="/register" element={<CreateStudentAccount />} />
       <Route path="/otp" element={<OTP />} />
 
       <Route path="/home" element={<HomePage />} />
-      <Route path="/tutors" element={<TutorPage />} />
+      <Route path="/tutors" element={<TutorsPage />} />
       <Route path="/favorites" element={<FavPage />} />
       <Route path="/notifications" element={<Notifications />} />
       <Route path="/profile" element={<Profile />} />
