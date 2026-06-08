c3a951298600b93f49cdf1c527fd110432b94c7e
huda <hudaaltabbal433@gmail.com>
2026-06-09 00:51:50 +0300

    committttt

--- Alef/src/api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://murmuring-cassette-jingling.ngrok-free.dev",
-});
-
-export default api;
--- Alef/src/components/tabs/Requests.jsx
+++
@@ -1,9 +0,0 @@
-import '../../styles/tstyle/Requests.css'
-
-export default function Requests(){
-    return(
-        <div>
-            
-        </div>
-    )
-}
